f577434f9a92301cb508eb27c52caf1d19614393
Mohamed <root@Debian>
2014-01-29 12:06:02 -0800

    Initial Commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+  def index
+    #flash[:notice] = "Login success"
+    #flash[:error] = "Invalid Login Credentials"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+RailsBootstrap::Application.routes.draw do
+  root :to => 'home#index'
+end
